53cfaea6cc6e440cd5b9aa324ac0e56acffef726
Mischa Alff <mischa@mutate.se>
2022-04-28 12:57:42 +0200

    Add action for determining environment variables required for deploying mutate-engine

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+	let environmentName = 'local';
+	let deployEnvironmentName = null;
+	if (github.context.ref == 'refs/heads/main') {
+		environmentName = 'dev';
+		deployEnvironmentName = 'dev-cb';
+	} else if (github.context.ref == 'refs/heads/innovate') {
+		environmentName = 'innovate';
+		deployEnvironmentName = 'innovate';
+	} else if (github.context.ref.startsWith('refs/heads/release/')) {
+		environmentName = 'stage';
+		deployEnvironmentName = 'stage';
+	} else if (github.context.ref.startsWith('refs/tags/')) {
+		environmentName = 'prod';
+		deployEnvironmentName = 'prod';
+	}
+
+	let shouldDeploy = false;
+	if (deployEnvironmentName != null && github.eventName == 'push') {
+		shouldDeploy = true;
+	}
+
+	core.setOutput('shouldDeploy', shouldDeploy);
+	core.setOutput('deployEnvironmentName', deployEnvironmentName);
+	core.setOutput('environmentName', environmentName);
+} catch (error) {
+	core.setFailed(error.message);
+}
